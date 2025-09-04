090acea0b03123ec64ef94466b6c2d9474a1b79d
Pablo Muris <PabloMuris@protonmail.com>
2025-09-04 11:16:33 -0300

    Refactor base and index templates for improved structure and styling
    - Updated base.html to include new block for header and scripts, enhancing modularity.
    - Changed navigation link in base.html to point to 'locais' URL.
    - Added multiple CSS files in index.html for better styling organization.
    - Replaced meteorological card structure in index.html with a new banner layout.
    - Removed outdated meteorological alert card and unnecessary sections in index.html.

--- koltagri/landplots/urls.py
+++ koltagri/landplots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,re_path
-from .views import CultivatesView,CultivatesDetailView
+from .views import CultivatesView,CultivatesDetailView,SiteDetailView
 urlpatterns = [
-    path('',CultivatesView.as_view(),name='cultivates'),
+    path('locais',CultivatesView.as_view(),name='locais'),
     path('detalhes',CultivatesDetailView.as_view(),name='cultivates_detail'),
+    path("site/name",SiteDetailView.as_view(),name="site_detail")
 ]
--- koltagri/landplots/views.py
+++ koltagri/landplots/views.py
@@ -6,7 +6,10 @@ from django.views.generic import TemplateView
 
 
 class CultivatesView(TemplateView):
-    template_name = 'cultivos.html'
+    template_name = 'sites.html'
 
 class CultivatesDetailView(TemplateView):
     template_name = 'cultivate_detail.html'
+
+class SiteDetailView(TemplateView):
+    template_name = 'site/site_detail.html'
